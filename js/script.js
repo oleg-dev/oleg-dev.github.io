b150415036f3d1c5e984db275298accc90898a14
oleg-dev <oleg.merg.develop@gmail.com>
2017-01-26 01:19:24 +0200

    sdfdsaf

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+	$("#menu-footer").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+	$("#sandwich-menu").on("click","a", function (event) {
+		$('.nav-2').hide();
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+	$('.nav-2').hide();
+	$('#mobile-menu').click(function(){$(this).next().toggle()});
+});
